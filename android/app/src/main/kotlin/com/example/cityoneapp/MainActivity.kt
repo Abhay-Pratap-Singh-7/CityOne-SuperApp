11b1c3c72b02c41d59074246c3f7eaec315193f0
Abhay-Pratap-Singh-7 <abhaypratapsingh2993@gmail.com>
2026-06-09 11:50:56 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/cityoneapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cityoneapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CityOne());
+}
+
+class CityOne extends StatelessWidget {
+  const CityOne({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
